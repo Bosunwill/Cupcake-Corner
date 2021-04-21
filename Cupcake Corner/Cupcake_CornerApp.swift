eb3e17267231ec474f8c4ad06da8cc0178b30b3c
Bosunwill <54271663+Bosunwill@users.noreply.github.com>
2021-04-21 08:36:01 -0500

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Gui Ndikum on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
